22eb9e28e3069156e2ac7a490a6e43f4665ec966
son.nguyen2 <son.nguyen2@drjoy.vn>
2021-03-28 10:39:29 +0700

    1

---
+++ tts.service.nts.js
@@ -0,0 +1,20 @@
+let URL_TTS_SERVER = 'http://localhost:8080'; // Allow editing url!
+function textToSpeech(textInput, language) {
+  if (!textInput) {
+    alert('Empty input!')
+  } else {
+    if (!language) {
+      playAudioFromUrl(URL_TTS_SERVER+`/audio/FEMALE/JA_JP/${textInput}.mp3`);
+    } else {
+      playAudioFromUrl(URL_TTS_SERVER+`/audio/FEMALE/VI_VN/${textInput}.mp3`);
+    }
+  }
+}
+function playAudioFromUrl(audioUrl) {
+  const audioElm = new Audio(audioUrl);
+  audioElm.play().then(audioPlayResult => {
+    console.log(`Played audio from URL:${audioUrl}`);
+  }).catch(audioPlayError => {
+    console.warn(`Cannot play audio from URL:${audioUrl}`);
+  });
+}
