aba6eec329d984a6f6eb89bb3a4b4a208e0b215d
son.nguyen2 <son.nguyen2@drjoy.vn>
2021-04-04 06:22:25 +0700

    3--autospeak

--- tts.service.nts.js
+++ tts.service.nts.js
@@ -4,9 +4,9 @@ function textToSpeech(textInput, language) {
     alert('Empty input!')
   } else {
     if (!language) {
-      playAudioFromUrl(`/audio/FEMALE/JA_JP/${textInput}.mp3`);
+      playAudioFromUrl(`audio/FEMALE/JA_JP/${textInput}.mp3`);
     } else {
-      playAudioFromUrl(`/audio/FEMALE/VI_VN/${textInput}.mp3`);
+      playAudioFromUrl(`audio/FEMALE/VI_VN/${textInput}.mp3`);
     }
   }
 }
@@ -15,7 +15,6 @@ function playAudioFromUrl(audioUrl) {
   audioElm.play().then(audioPlayResult => {
     console.log(`Played audio from URL:${audioUrl}`);
   }).catch(audioPlayError => {
-    console.warn(audioPlayError);
     console.warn(`Cannot play audio from URL:${audioUrl}`);
   });
 }
